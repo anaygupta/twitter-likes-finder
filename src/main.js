5ee31f78c931dd466efb2d516089217316ce3196
anaygupta <anay0@icloud.com>
2022-05-29 12:20:51 -0700

    add: search input

--- src/main.js
+++ src/main.js
@@ -4,27 +4,10 @@ import "./index.css";
 import router from "./router";
 import store from "./store";
 
-// import google oauth library
-import GAuth from "vue3-google-oauth2";
-const gAuthOptions = {
-  clientId:
-    "230387097801-la30otst77ac3fog9d5uhtqm6o672h76.apps.googleusercontent.com",
-  scope: [
-    "openid",
-    "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "https://mail.google.com/",
-    "https://www.googleapis.com/auth/calendar",
-    "https://www.googleapis.com/auth/contacts",
-  ],
-  prompt: "consent",
-  fetch_basic_profile: true,
-};
 
 // setting up app
 const app = createApp(App);
 app.use(router);
 app.use(store);
-app.use(GAuth, gAuthOptions);
 // mount the app
 app.mount("#app");
--- src/views/Public/Product.vue
+++ src/views/Public/Product.vue
@@ -1,9 +1,29 @@
 <template>
   <div id="product">
-    <h1>product page</h1>
+    <h1 class="text-xl font-bold">Search your Twitter Likes</h1>
+    <div class="search-input w-72 m-auto">
+        <input v-model="searchInput" type="text" placeholder="Search" class="mt-5 w-full border border-slate-400 text-md rounded-md px-2 py-1" @keyup.enter="searchLikes" />
+    </div>
+    <div class="results my-10">
+        {{ message }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            searchInput: "",
+            message: ""
+        }
+    },
+    methods:  {
+        searchLikes() {
+            if (this.searchInput) {
+                this.message = 'enter was hit'
+            }
+        }
+    }
+};
 </script>
